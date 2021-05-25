5bd1b34065ba13409e73a5031eae19de420ff941
Adrián <statickidz@gmail.com>
2021-05-25 21:15:03 +0200

    feat: readme, beauty logs, env config

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pumperox
+
+## Installation
+
+```
+npm install
+```
+
+## Usage
+
+- Rename `.env.sample` to `.env` and customize `WATCH_ADDRESS`.
+
+- Start watching
+
+```
+npm start
+```
--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+require('dotenv').config();
 const Web3 = require('web3');
 const open = require('open');
 const InputDataDecoder = require('ethereum-input-data-decoder');
 const decoder = new InputDataDecoder(`${__dirname}/abi.json`);
 
 // testnet
-const web3 = new Web3('wss://bsc.getblock.io/testnet/?api_key=00854c01-32fa-4602-b14a-7b0b2e54650e');
+//const web3 = new Web3('wss://bsc.getblock.io/testnet/?api_key=00854c01-32fa-4602-b14a-7b0b2e54650e');
 
 // mainnet 
-//const web3 = new Web3('wss://bsc.getblock.io/mainnet/?api_key=00854c01-32fa-4602-b14a-7b0b2e54650e');
+const web3 = new Web3('wss://bsc.getblock.io/mainnet/?api_key=00854c01-32fa-4602-b14a-7b0b2e54650e');
 
-// test account 1: 0x462C4b17eAb238e07d04F9776470517Cf7eed6f3
-// test account 2: 0x4695471820b70f2309F87DF9936cbdA08b764AaB
+let address = process.env.WATCH_ADDRESS;
 
-let address = '0x462C4b17eAb238e07d04F9776470517Cf7eed6f3';
-
+console.log(`🟢 Watching ${address} ...`);
 web3.eth.subscribe('pendingTransactions', (err, txHash) => {
   if (err) {
+    console.log(`🔴 Error retrieving network pending transactions`);
     throw (err);
   }
 })
   .on('data', function (txHash) {
     return web3.eth.getTransaction(txHash, async (err, transaction) => {
       if (err) {
+        console.log(`🔴 ${txHash} not valid transaction`);
         throw (err);
       }
       if (transaction && (transaction.from === address || transaction.to === address)) {
@@ -30,8 +31,8 @@ web3.eth.subscribe('pendingTransactions', (err, txHash) => {
           const routes = result.inputs[2];
           const tokenAddress = `0x${routes[routes.length - 1]}`;
 
-          console.log(`TransactionHash: ${transaction.hash}`);
-          console.log(`Token Address: ${tokenAddress}`);
+          console.log(`🚩 TransactionHash: ${transaction.hash}`);
+          console.log(`🚩 Token Address: ${tokenAddress}`);
 
           await open(`https://poocoin.app/tokens/${tokenAddress}`);
           await open(`https://exchange.pancakeswap.finance/#/swap?outputCurrency=${tokenAddress}`);
